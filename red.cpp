a39969163cfbb2d9a9327d487061a11cf3761196
Aditya <raditya@iitk.ac.in>
2018-03-19 22:22:31 +0530

    Hysterysis thresholding, too slow

--- red.cpp
+++ red.cpp
@@ -7,21 +7,22 @@ using namespace std;
 
 int main( int argc, char** argv )
 {
-    Mat image,M2;
+    Mat image,image2;
     image = imread("./frame0001.jpg", CV_LOAD_IMAGE_COLOR);   // Read the file
-    Mat M ;
-
-    //cv::namedWindow("Original",1);
-    //cv::imshow("Original", image); 
+    //Mat image = image;
+    int width = image.size().width;
+    int height = image.size().height;
+    cv::namedWindow("Original",1);
+    cv::imshow("Original", image); 
     GaussianBlur( image, image, Size( 15, 15 ), 10, 10 );
 
     //cv::namedWindow("Blurred",1);
     //cv::imshow("Blurred", image); 
-    for(int i=0;i<720;i++){
-        for(int j=0;j<1280;j++){
+    /*for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
             int blue = image.at<cv::Vec3b>(i,j)[0]; 
 
-            if(blue>155){
+            if(blue>75){
             image.at<cv::Vec3b>(i,j)[0] = 255;
             image.at<cv::Vec3b>(i,j)[1] = 255;
             image.at<cv::Vec3b>(i,j)[2] = 255;
@@ -32,23 +33,57 @@ int main( int argc, char** argv )
             image.at<cv::Vec3b>(i,j)[2] = 0;
         	}
         }
+    }*/
+    queue < pair<int, int> > ColorBox;
+    int cnt=0;
+    for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+            int blue = image.at<cv::Vec3b>(i,j)[0]; 
+
+            if(blue<85){
+            	image.at<cv::Vec3b>(i,j)[0] = 0;
+            	image.at<cv::Vec3b>(i,j)[1] = 0;
+            	image.at<cv::Vec3b>(i,j)[2] = 0;
+          }
+            else if(blue>135){
+           		//pair<int,int> temp = make_pair(i,j);
+          		ColorBox.push(make_pair(i,j));
+            	image.at<cv::Vec3b>(i,j)[0] = 255;
+            	image.at<cv::Vec3b>(i,j)[1] = 255;
+            	image.at<cv::Vec3b>(i,j)[2] = 255;
+          }
+          else{
+          		image.at<cv::Vec3b>(i,j)[0] = 1;
+            	image.at<cv::Vec3b>(i,j)[1] = 1;
+            	image.at<cv::Vec3b>(i,j)[2] = 1;
+          }
+        }
     }
+    while(!ColorBox.empty()){
+          		cnt++;
+    	pair<int,int> var = ColorBox.front();
+    	ColorBox.pop();
+    	int arr1[] = {-1,0,1};
+    	int arr2[] = {-1,0,1};
+    	for(int i=0;i<3;i++){
+    		for(int j=0;j<3;j++){
+    			if(arr1[i]==0 && arr2[j]==0) continue;
+    			if(!(var.first + arr1[i] < 0|| var.first + arr1[i] >= height || var.second + arr2[j]<0 || var.second + arr2[j]>=width)){
+    				if(image.at<cv::Vec3b>(var.first + arr1[i], var.second + arr2[j])[0] == 1){
+    				image.at<cv::Vec3b>(var.first + arr1[i], var.second + arr2[j])[0] = 255;
+          			ColorBox.push(make_pair(var.first + arr1[i], var.second + arr2[j]));
+    			}
+    			else
+    				image.at<cv::Vec3b>(var.first + arr1[i], var.second + arr2[j])[0] = 0;
+    			}
+    			//pair<int,int> temp1 = make_pair(var.first + arr1[i], var.second + arr2[j]);
+    		}
+    	}
+    }
+    cout<<cnt<<endl;
     cv::cvtColor(image, image, CV_RGB2GRAY);
-	/*vector<vector<Point> > contours;
-	vector<Vec4i> hierarchy;
-	findContours( image, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-	double min_area=10000; // area threshold
- 	double max_area=20000; // area threshold
- 	for( int i = 0; i< contours.size(); i++ ) // iterate through each contour.
- 	{
- 	  double area=contourArea( contours[i],false);  //  Find the area of contour
- 	  if(area<min_area)
- 	  	contours.erase(contours.begin() + i);
- 	  
-		drawContours( image, contours, i, Scalar(0,0,255), 2, 8, hierarchy, 0, Point() ); 	
-	}*/
 	cv::Mat structuringElement = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(40, 40));
-cv::morphologyEx( image, image, cv::MORPH_CLOSE, structuringElement );
+	cv::morphologyEx( image, image, cv::MORPH_CLOSE, structuringElement );
     cv::namedWindow("Normal",1);
     cv::imshow("Normal", image);    
     waitKey(0);//Wait for a keystroke in the window
